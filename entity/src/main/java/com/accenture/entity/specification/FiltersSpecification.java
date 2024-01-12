761dba1d5d3b4d9c5b3b932f921464188eee53e7
Mikołaj Telus <telusmikolaj@gmail.com>
2024-01-12 15:11:37 +0100

    generic specification with joins

--- entity/src/main/java/com/accenture/entity/specification/FiltersSpecification.java
+++ entity/src/main/java/com/accenture/entity/specification/FiltersSpecification.java
@@ -2,60 +2,92 @@ package com.accenture.entity.specification;
 
 import com.accenture.api.form.RequestSearchForm;
 import com.accenture.api.form.SearchRequestDTO;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FiltersSpecification<T> {
 
-    public Specification<T> getSearchSpecification(SearchRequestDTO searchRequestDTO) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(searchRequestDTO.getColumn()), searchRequestDTO.getValue());
-    }
+    public static final String LIKE_OPERATOR = "%";
+    private static final String COMMA_REGEX = ",";
 
     public Specification<T> getSearchSpecification(List<SearchRequestDTO> searchRequestDtos, RequestSearchForm.GlobalOperator globalOperator) {
+        if (searchRequestDtos.isEmpty()) {
+            return (root, query, criteriaBuilder) -> criteriaBuilder.conjunction();
+        }
+
         return (root, query, criteriaBuilder) -> {
-            List<Predicate> predicates = new ArrayList<>();
+            List<Predicate> predicates = searchRequestDtos.stream()
+                    .map(dto -> createPredicateFromDto(dto, root, criteriaBuilder))
+                    .toList();
 
-            for (SearchRequestDTO searchRequestDto : searchRequestDtos) {
-
-                predicates.add(createPredicateFromDto(searchRequestDto, root, criteriaBuilder));
-            }
-
-            if (globalOperator.equals(RequestSearchForm.GlobalOperator.AND)) {
-                return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
-            } else {
-                return criteriaBuilder.or(predicates.toArray(new Predicate[0]));
-            }
+            Predicate[] predicateArray = predicates.toArray(Predicate[]::new);
+            return globalOperator.equals(RequestSearchForm.GlobalOperator.AND) ?
+                    criteriaBuilder.and(predicateArray) :
+                    criteriaBuilder.or(predicateArray);
         };
     }
 
-    private Predicate createPredicateFromDto(SearchRequestDTO searchRequestDto, Root<T> root, CriteriaBuilder criteriaBuilder) {
+    private Predicate createPredicateFromDto(SearchRequestDTO dto, Root<T> root, CriteriaBuilder criteriaBuilder) {
+        Path<?> path = Optional.ofNullable(dto.getJoinTable())
+                .filter(joinTable -> !joinTable.isEmpty())
+                .map(joinTable -> createJoinPath(root, joinTable, dto.getJoinField()))
+                .orElseGet(() -> root.get(dto.getColumn()));
+
+        return createPredicate(path, dto, criteriaBuilder);
+    }
+
+    private Path<?> createJoinPath(Root<T> root, String joinTable, String joinField) {
+        Join<Object, Object> join = root.join(joinTable, JoinType.LEFT);
+        return join.get(joinField);
+    }
+
+    private Predicate createPredicate(Path<?> path, SearchRequestDTO searchRequestDto, CriteriaBuilder criteriaBuilder) {
         return switch (searchRequestDto.getOperation()) {
-            case EQUAL -> criteriaBuilder.equal(root.get(searchRequestDto.getColumn()), searchRequestDto.getValue());
-            case LIKE ->
-                    criteriaBuilder.like(root.get(searchRequestDto.getColumn()), "%" + searchRequestDto.getValue() + "%");
-            case IN -> {
-                String[] splitIn = searchRequestDto.getValue().split(", ");
-                yield root.get(searchRequestDto.getColumn()).in(Arrays.asList(splitIn));
-            }
-            case GREATER_THAN ->
-                    criteriaBuilder.greaterThan(root.get(searchRequestDto.getColumn()), searchRequestDto.getValue());
-            case LESS_THAN ->
-                    criteriaBuilder.lessThan(root.get(searchRequestDto.getColumn()), searchRequestDto.getValue());
-            case BETWEEN -> {
-                String[] splitBetween = searchRequestDto.getValue().split(",");
-                yield criteriaBuilder.between(root.get(searchRequestDto.getColumn()), splitBetween[0], splitBetween[1]);
-            }
-            case JOIN ->
-                    criteriaBuilder.equal(root.join(searchRequestDto.getJoinTable()).get(searchRequestDto.getColumn()), searchRequestDto.getValue());
+            case EQUAL -> createEqualPredicate(path, searchRequestDto, criteriaBuilder);
+            case LIKE -> createLikePredicate(path, searchRequestDto, criteriaBuilder);
+            case IN -> createInPredicate(path, searchRequestDto);
+            case GREATER_THAN -> createGreaterThanPredicate(path, searchRequestDto, criteriaBuilder);
+            case LESS_THAN -> createLessThanPredicate(path, searchRequestDto, criteriaBuilder);
+            case BETWEEN -> createBetweenPredicate(path, searchRequestDto, criteriaBuilder);
+            default ->
+                    throw new UnsupportedOperationException("Operation " + searchRequestDto.getOperation() + " is not supported.");
         };
     }
+
+    private Predicate createEqualPredicate(Path<?> path, SearchRequestDTO searchRequestDto, CriteriaBuilder criteriaBuilder) {
+        return criteriaBuilder.equal(path, searchRequestDto.getValue());
+    }
+
+    private Predicate createLikePredicate(Path<?> path, SearchRequestDTO searchRequestDto, CriteriaBuilder criteriaBuilder) {
+        return criteriaBuilder.like(path.as(String.class), LIKE_OPERATOR + searchRequestDto.getValue() + LIKE_OPERATOR);
+    }
+
+    private Predicate createInPredicate(Path<?> path, SearchRequestDTO searchRequestDto) {
+        String[] splitIn = searchRequestDto.getValue().split(", ");
+        return path.in(Arrays.asList(splitIn));
+    }
+
+    private Predicate createGreaterThanPredicate(Path<?> path, SearchRequestDTO searchRequestDto, CriteriaBuilder criteriaBuilder) {
+        return criteriaBuilder.greaterThan(path.as(String.class), searchRequestDto.getValue());
+    }
+
+    private Predicate createLessThanPredicate(Path<?> path, SearchRequestDTO searchRequestDto, CriteriaBuilder criteriaBuilder) {
+        return criteriaBuilder.lessThan(path.as(String.class), searchRequestDto.getValue());
+    }
+
+    private Predicate createBetweenPredicate(Path<?> path, SearchRequestDTO searchRequestDto, CriteriaBuilder criteriaBuilder) {
+        String[] splitBetween = searchRequestDto.getValue().split(COMMA_REGEX);
+        if (splitBetween.length < 2) {
+            throw new IllegalArgumentException("BETWEEN operation requires two parameters.");
+        }
+        return criteriaBuilder.between(path.as(String.class), splitBetween[0], splitBetween[1]);
+    }
+
 
 }
