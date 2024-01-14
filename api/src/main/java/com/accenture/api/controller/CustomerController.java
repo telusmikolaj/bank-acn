1d3c69f34c41b451307f54468745e7012ef6695c
Mikołaj Telus <telusmikolaj@gmail.com>
2024-01-14 08:24:54 +0100

    controller refactor: - delete httpResponse

--- api/src/main/java/com/accenture/api/controller/CustomerController.java
+++ api/src/main/java/com/accenture/api/controller/CustomerController.java
@@ -4,19 +4,9 @@ import com.accenture.api.dto.CustomerDTO;
 import com.accenture.api.form.CustomerForm;
 import com.accenture.api.form.RequestSearchForm;
 import com.accenture.api.service.CustomerService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import lombok.RequiredArgsConstructor;import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Map;
-
-import static java.time.LocalTime.now;
-import static java.util.Map.of;
-import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping(path = "/customer")
@@ -26,11 +16,9 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping
-    public ResponseEntity<HttpResponse> create(@RequestBody CustomerForm customerForm) {
-        CustomerDTO created = this.customerService.create(customerForm);
-        return ResponseEntity
-                .created(getUri())
-                .body(createHttpResponse(CREATED, of("customer", created), "Customer created"));
+    public CustomerDTO create(@RequestBody CustomerForm customerForm) {
+        return this.customerService.create(customerForm);
+
     }
 
     @GetMapping
@@ -38,20 +26,5 @@ public class CustomerController {
         return this.customerService.searchCustomers(requestForm);
 
     }
-
-    private HttpResponse createHttpResponse(HttpStatus httpStatus, Map<?, ?> data, String message) {
-        return HttpResponse.builder()
-                .timeStamp(now().toString())
-                .data(data)
-                .message(message)
-                .status(httpStatus)
-                .statusCode(httpStatus.value())
-                .build();
-    }
-
-    private URI getUri() {
-        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/customer/get/<customerId>").toUriString());
-    }
-
 
 }
